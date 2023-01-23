13c8095ef87026fda39f0385601cb83a3c50ae2e
Charles-04 <charlesmir04@gmail.com>
2023-01-23 18:17:49 +0100

    Fix : Removed all empty Json Objects

--- CustomAttributes/Attribute.Implementation.cs
+++ CustomAttributes/Attribute.Implementation.cs
@@ -1,140 +1,118 @@
 ﻿using System;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Text.Json;
 using System.Collections.Generic;
 using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace CustomAttributes
 {
 
     public static class Implementation
     {
-       static List<dynamic> _jsonFormatDocs = new List<dynamic>();
+        static List<dynamic> _jsonFormatDocs = new List<dynamic>();
 
-        public  async static Task  GenerateTextDoc()
+       
+
+        public static void GenerateDocuments()
         {
-            
-
-            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
-            var members = from assembly in assemblies
-                          from type in assembly.GetTypes()
-                          from member in type.GetMembers()
-                          select member;
-
-
-           
-           await using (StreamWriter writer = File.CreateText(@"Documentation.txt"))
-            {
-                
-                foreach (var member in members)
-                {
-
-                    var attributes = member.GetCustomAttributes(typeof(DocumentAttribute), true);
-                    var clasAttributes = member.DeclaringType.GetCustomAttributes(typeof(DocumentAttribute), true);
-                    if (attributes.Length > 0)
-                    {
-                        var doc = (DocumentAttribute)attributes[0];
-                        writer.WriteLine($"\nClass : {member.DeclaringType.Name}\n");
-                        if (clasAttributes.Length > 0)
-                        {
-                            var classDoc = (DocumentAttribute)clasAttributes[0];
-                            writer.WriteLine($"Description : {classDoc.Description}\n");
-                        }
-
-                        writer.WriteLine($"Member Type: {member.MemberType}");
-                        writer.WriteLine($"Name: {member.Name}");
-
-                        writer.WriteLine($"\nDescription: {doc.Description}\n");
-                        if (doc.Input != null)
-                        {
-                            writer.WriteLine($"Input: {doc.Input}");
-                        }
-                        if (doc.Output != null)
-                        {
-                            writer.WriteLine($"\nOutput: {doc.Output}\n");
-                        }
-
-                    }
-
-                }
-
-                Console.WriteLine(@"Documentation created at the folder : C:\Users\Charles\source\repos\DocumentationGenerator\DocGenerator\bin\Debug\net6.0");
-
-               
-            }
-
-
-        }
-
-        public static void GenerateJson()
-        {
-           // await GenerateTextDoc();
+            // await GenerateTextDoc();
             var assemblies = AppDomain.CurrentDomain.GetAssemblies();
             var types = from _assembly in assemblies
-                           from _types in _assembly.GetTypes()
-                          select _types;
-
-            foreach (var item in types)
+                        from _types in _assembly.GetTypes()
+                        select _types;
+            using (StreamWriter writer = File.CreateText(@"Documentation.txt"))
             {
-                var members = item.GetMembers();
-                for (int i = 0; i < members.Length; i++)
+                foreach (var item in types)
                 {
-                    _jsonFormatDocs.Add(new ExpandoObject());
-
-                    var attributes = members[i].GetCustomAttributes(typeof(DocumentAttribute), true);
-                    var clasAttributes = members[i].DeclaringType.GetCustomAttributes(typeof(DocumentAttribute), true);
-                    if (attributes.Length > 0)
+                    var members = item.GetMembers();
+                    for (int i = 0; i < members.Length; i++)
                     {
-                        var doc = (DocumentAttribute)attributes[0];
-                        _jsonFormatDocs[i].ClassName = $"\nClass : {members[i].DeclaringType.Name}\n";
 
-                        if (clasAttributes.Length > 0)
-                        {
-                            var classDoc = (DocumentAttribute)clasAttributes[0];
-                            _jsonFormatDocs[i].Description = ($"Description : {classDoc.Description}\n");
-                        }
 
-                        _jsonFormatDocs[i].MemberType = ($"Member Type: {members[i].MemberType}");
-                        _jsonFormatDocs[i].MemberName = ($"Name: {members[i].Name}");
 
-                        _jsonFormatDocs[i].MemberDescription = ($"\nDescription: {doc.Description}\n");
-                        if (doc.Input != null)
+                        var attributes = members[i].GetCustomAttributes(typeof(DocumentAttribute), true);
+                        var clasAttributes = members[i].DeclaringType.GetCustomAttributes(typeof(DocumentAttribute), true);
+
+                       
+
+
+
+                        if (attributes.Length > 0 && members[i] != null)
                         {
-                            _jsonFormatDocs[i].Input = ($"Input: {doc.Input}");
-                        }
-                        if (doc.Output != null)
-                        {
-                            _jsonFormatDocs[i].Output = ($"\nOutput: {doc.Output}\n");
+                            _jsonFormatDocs.Add(new ExpandoObject());
+                            var lastIndex = _jsonFormatDocs.Count() - 1;
+                            
+                            var doc = (DocumentAttribute)attributes[0];
+                            _jsonFormatDocs[lastIndex].ClassName = $"\nClass : {members[i].DeclaringType.Name}\n";
+                            writer.WriteLine($"\nClass : {members[i].DeclaringType.Name}\n");
+
+                            if (clasAttributes.Length > 0)
+                            {
+                                var classDoc = (DocumentAttribute)clasAttributes[0];
+                                _jsonFormatDocs[lastIndex].Description = ($"Description : {classDoc.Description}\n");
+                                writer.WriteLine($"Description : {classDoc.Description}\n");
+                            }
+
+                            _jsonFormatDocs[lastIndex].MemberType = ($"Member Type: {members[i].MemberType}");
+                            writer.WriteLine($"Member Type: {members[i].MemberType}");
+
+                            _jsonFormatDocs[lastIndex].MemberName = ($"Name: {members[i].Name}");
+                            writer.WriteLine($"Name: {members[i].Name}");
+
+                            _jsonFormatDocs[lastIndex].MemberDescription = ($" {doc.Description}");
+                            writer.WriteLine($" {doc.Description}");
+                            if (doc.Input != null)
+                            {
+                                _jsonFormatDocs[lastIndex].Input = ($"Input: {doc.Input}");
+                                writer.WriteLine($"Input: {doc.Input}");
+                            }
+                            if (doc.Output != null)
+                            {
+                                _jsonFormatDocs[lastIndex].Output = ($"{doc.Output}");
+                                writer.WriteLine($"\nOutput: {doc.Output}\n");
+                            }
+
                         }
 
                     }
-
                 }
 
                
-
-
             }
 
-            JsonSerialization.SaveAsJsonFormat(_jsonFormatDocs, "Document.json");
-            Console.WriteLine("Done");
-        }
-        }
-    }
-    public static class JsonSerialization
-    {
-        public static void SaveAsJsonFormat<T>(T objGraph, string fileName)
-        {
-            var options = new JsonSerializerOptions
+            /*var jsonDoc = new List<dynamic>();
+            for (int i = 0; i < _jsonFormatDocs.Count(); i++)
             {
-                IncludeFields = true,
-                WriteIndented = true
-            };
-            File.WriteAllText(fileName, JsonSerializer.Serialize(objGraph, options));
+                if (!((IDictionary<String, Object>)_jsonFormatDocs[i]).ContainsKey("Class"))
+                {
+                    _jsonFormatDocs.Remove(_jsonFormatDocs[i]);
+                }
+                else
+                {
+                    jsonDoc.Add(_jsonFormatDocs[i]);
+                }
+            }*/
+            JsonSerialization.SaveAsJsonFormat(_jsonFormatDocs, "Document.json");
+            Console.WriteLine("Documentation created in Json and txt format");
+
+
         }
     }
+}
+public static class JsonSerialization
+{
+    public static void SaveAsJsonFormat<T>(T objGraph, string fileName)
+    {
+        var options = new JsonSerializerOptions
+        {
+            IncludeFields = true,
+            WriteIndented = true
+        };
+        File.WriteAllText(fileName, JsonSerializer.Serialize(objGraph, options));
+    }
+}
 
 
 
--- DocGenerator/Generator.cs
+++ DocGenerator/Generator.cs
@@ -1,8 +1,9 @@
 ﻿using CustomAttributes;
-using System;
+using System.Dynamic;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace DocGenerator
@@ -11,7 +12,16 @@ namespace DocGenerator
     {
         public static void GetDocs()
         {
-             Implementation.GenerateJson();
-        }
+            //Implementation.GenerateDocuments();
+            var docs = JsonDeserialization.ReadAsJsonFormat<dynamic>("Document.json");
+           Console.WriteLine(docs);
+           // var txt = File.ReadAllText("Documentation.txt");
+           // Console.WriteLine(txt.ToString());
+    }       
+    }
+    public static class JsonDeserialization
+    {
+        public static T ReadAsJsonFormat<T>(string fileName) =>
+            JsonSerializer.Deserialize<T>(File.ReadAllText(fileName));
     }
 }
